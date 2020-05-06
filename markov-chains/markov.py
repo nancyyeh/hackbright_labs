92237ed83acafc9f4343ee3aa9dec97971a3781b
Nancy Yeh <heyycnan@gmail.com>
2020-05-05 19:19:11 -0700

    working markov-chains

--- markov-chains/markov.py
+++ markov-chains/markov.py
@@ -2,7 +2,6 @@
 
 from random import choice
 
-
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
 
@@ -11,10 +10,11 @@ def open_and_read_file(file_path):
     """
 
     # your code goes here
-    text = open(file_path)
+    text = open(file_path).read()
+    # text = text.replace("\n", " ") # replace /n with spaces
+    # text = text.rstrip() #remove the last space
 
-
-    return text.read()
+    return text
 
 
 def make_chains(text_string):
@@ -43,26 +43,53 @@ def make_chains(text_string):
     """
 
     chains = {}
+    text_string = text_string.split(" ")
 
     # your code goes here
+    for index in range(len(text_string)-1):
+        tup = (text_string[index], text_string[index+1])
+        if index < len(text_string)-2:
+            chains[tup] = chains.get(tup, [])
+            word_following = text_string[index+2]
+            chains[tup].append(word_following)
 
+    # print(chains)
     return chains
 
 
 def make_text(chains):
     """Return text from chains."""
-
     words = []
 
     # your code goes here
 
+    list_keys = list(chains.keys())
+    
+
+    starting_key = choice(list_keys)
+    # append the two starting key
+    words.append(starting_key[0])
+    words.append(starting_key[1])
+
+    while True:
+        current_key = tuple(words[-2:])
+        # print (current_key)
+        if chains.get(current_key) is None:
+            break
+        else:
+            next_word = choice(chains[current_key])
+            words.append(next_word)
+    
     return " ".join(words)
 
 
 input_path = "green-eggs.txt"
+# input_text = "hi there mary hi there juanita"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
+
+print(input_text)
 
 # Get a Markov chain
 chains = make_chains(input_text)

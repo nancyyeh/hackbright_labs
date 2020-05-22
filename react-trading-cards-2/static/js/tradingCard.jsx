f8b33e4bf81b76548bab91bd0134e4cbd584b15e
Nancy Yeh <heyycnan@gmail.com>
2020-05-22 16:09:50 -0700

    finished lab 0522

--- react-trading-cards-2/static/js/tradingCard.jsx
+++ react-trading-cards-2/static/js/tradingCard.jsx
@@ -1,48 +1,64 @@
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg'
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
   },
 
   {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
   },
 
   {
-    name: 'Llambda',
-    skill: 'knitting scarves',
-    imgUrl: '/static/img/llambda.jpg'
-  }
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+  },
 ];
 
 class TradingCard extends React.Component {
   render() {
     return (
       <div className="card">
-        <p class="card-name">
-          Name: {this.props.name}
-        </p>
+        <p class="card-name">Name: {this.props.name}</p>
 
         <div class="card-img">
           <img src={this.props.imgUrl} />
         </div>
 
-        <p class="card-details">
-          Skill: {this.props.skill}
-        </p>
+        <p class="card-details">Skill: {this.props.skill}</p>
       </div>
     );
   }
 }
 
 class TradingCardContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { cards: [] };
+    this.updateCards = this.updateCards.bind(this);
+  }
+
+  updateCards(response) {
+    const cards = response.cards;
+    this.setState({ cards: cards });
+  }
+
+  getAllCards() {
+    $.get("/api/cards", this.updateCards);
+  }
+
+  componentDidMount() {
+    this.getAllCards();
+  }
+
   render() {
     const tradingCards = [];
 
-    for (const currentCard of tradingCardData) {
+    for (const currentCard of this.state.cards) {
       tradingCards.push(
         <TradingCard
           key={currentCard.name}
@@ -53,13 +69,8 @@ class TradingCardContainer extends React.Component {
       );
     }
 
-    return (
-      <div id="container">{tradingCards}</div>
-    );
+    return <div id="container">{tradingCards}</div>;
   }
 }
 
-ReactDOM.render(
-  <TradingCardContainer />,
-  document.getElementById('app')
-);
+ReactDOM.render(<TradingCardContainer />, document.getElementById("app"));
